fe2964be004e807c8f09344d13ab1c84ab5a228b
Alex_Mitchell <ebpvyeuj@gmail.com>
2022-01-08 13:08:53 -0800

    Minor change to demo.py for runtime clarity.
    - Added a try/except block to handle a WorksheetNotFound error. As
      someone new to gspread trying to run the demo for the first time, I
      misunderstood the error I was getting and went down the wrong rabbit
      hole trying to fix a relatively simple issue.

--- demo.py
+++ demo.py
@@ -7,8 +7,11 @@ auth = gspread.oauth()
 # From the URL of the "Seacow market prototype v2" spreadsheet.
 doc = auth.open_by_key('126oXnb_MY7JUeRQpGgN6YVsGkKThBzf7udPuKhO1kzw')
 
-sheet = doc.worksheet("Hello World")
+sheet_name = "Hello World"
+try:
+    sheet = doc.worksheet(sheet_name)
+    print(f"Cell A1 contains: {sheet.acell('A1').value}")
 
-print(sheet.acell('A1').value)
-
-
+except gspread.exceptions.WorksheetNotFound:
+    print((f'Spreadsheet document "{doc.title}" '
+        'does not have the sheet "{sheet_name}"'))
