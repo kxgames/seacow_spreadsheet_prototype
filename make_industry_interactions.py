68b7087e1ca43854c9cd02ccccf6fb820187bb8d
Kale Kundert <kale@thekunderts.net>
2022-01-09 16:51:51 -0500

    feat: Add command-line interface to insteractions script

--- make_industry_interactions.py
+++ make_industry_interactions.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 
+"""\
+Usage:
+    make_industry_interactions.py [-r <seed>] [-U]
+
+Options:
+    -r --random-seed <int>      [default: 0]
+        Use the given random seed when generating the interaction graph.
+
+    -U --no-upload
+        Don't upload the resulting industry interactions to Google drive.
+"""
+
 # Right now, this script creates a graph by picking 2 random interactions (1 
 # positive, 1 negative) for each industry.  These interactions will probably be 
 # fairly ridiculous, but hopefully we'll at least see what kinds of graph 
@@ -14,6 +26,7 @@
 # would give us more knobs to control, but would also make the game more 
 # complicated.
 
+import docopt
 import seacow
 import random
 import networkx as nx
@@ -21,7 +34,10 @@ import matplotlib.pyplot as plt
 
 from math import *
 
-random.seed(1)
+args = docopt.docopt(__doc__)
+
+seed = int(args['--random-seed'])
+random.seed(seed)
 
 doc = seacow.load_doc()
 industries = seacow.load_industries(doc)
@@ -53,13 +69,18 @@ for i, name in enumerate(names):
 print(nx.to_pandas_adjacency(interactions).astype(int))
 
 # Upload the industry interactions:
-records = [
-        {'Industry 1': k1, 'Industry 2': k2, 'Interaction': v['weight']}
-        for (k1, k2), v in interactions.edges().items()
-]
-seacow.record_industry_interactions(doc, records)
+if not args['--no-upload']:
+    print("Uploading to Google drive...")
+    records = [
+            {'Industry 1': k1, 'Industry 2': k2, 'Interaction': v['weight']}
+            for (k1, k2), v in interactions.edges().items()
+    ]
+    seacow.record_industry_interactions(doc, records)
+else:
+    print("Skipped upload")
 
 # Plot the industry interactions:
 
 viz = nx.nx_agraph.to_agraph(interactions)
 viz.draw('industry_interactions.svg', prog='dot')
+print("Updated SVG file")
